09ee745b3bf8f80e26884b3a9ce5f2aed32bd569
Daniel Smith <smithdp60@gmail.com>
2015-06-07 01:35:00 -0700

    adding prod db

--- models/index.js
+++ models/index.js
@@ -23,14 +23,14 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
 var db        = {};
 
 fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename);
-  })
-  .forEach(function(file) {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+.readdirSync(__dirname)
+.filter(function(file) {
+  return (file.indexOf(".") !== 0) && (file !== basename);
+})
+.forEach(function(file) {
+  var model = sequelize["import"](path.join(__dirname, file));
+  db[model.name] = model;
+});
 
 Object.keys(db).forEach(function(modelName) {
   if ("associate" in db[modelName]) {
